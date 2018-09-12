6756b1cf2658ac93bc139cb49f3313ec34fd723e
li123139915 <123139915@qq.com>
2018-09-12 11:48:11 +0800

    第一次提交

---
+++ src/pages/index/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="header">
+      <icon type="search" size="20"></icon>
+      <input type="text" placeholder="搜索">
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+ 
+}
+</script>
+
+<style lang="less">
+    .header {
+      background-color: #f10250;
+      padding: 20rpx 16rpx;
+      position: relative;
+      icon{
+        position: absolute;
+        top: 34rpx;
+        left: 50%;
+        transform:translate(-200%);
+        z-Index: 998;
+      }
+      input {
+        height: 60rpx;
+        width: 100%;
+        background-color: #fff;
+        text-align:center;
+        border-radius: 6rpx;
+        font-size: 26rpx;
+      }
+    }
+</style>
